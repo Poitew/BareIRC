339bbf51622a75f52323b7e8d99766592e018a0c
Poitew <tommasoromano123@gmail.com>
2025-07-05 15:32:29 +0200

    fix: flake.nix

--- flake.nix
+++ flake.nix
@@ -3,16 +3,16 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    }
+    };
 
-    output: { self, nixpkgs }: let
+    outputs = { self, nixpkgs }: let
         system = "x86_64-linux";
         pkgs = nixpkgs.legacyPackages.${system};
     in {
         devShells.${system}.default = pkgs.mkShell {
             buildInputs = [
                 pkgs.cargo
-            ]
-        }
-    }
+            ];
+        };
+    };
 }
